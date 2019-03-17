d0f9d8325423b2aec1923d319d0c97b9530d6c69
Abimzy <eabiemo@gmail.com>
2019-03-16 20:18:54 -0700

    Defines POST, GET and DELETE action for todo app routes

--- Eunice-Abimzy/week-8/flask-react-todo-refactor/react-back-end/app.py
+++ Eunice-Abimzy/week-8/flask-react-todo-refactor/react-back-end/app.py
@@ -1,15 +1,51 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+CORS(app)
 
-DEBUG = True
+# Set Base Directory
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Setup Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.reacttodo')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Init Database
+db = SQLAlchemy(app)
+
+#Init Marshmallow
+marshmallow = Marshmallow(app)
+
+DEBUG = True 
 PORT = 8000
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+# @app.route('/todo', methods=['POST']
+
+
+@app.route('/todo', methods=['GET', 'POST'])
+@app.route('/todo/<todoid>', methods=['GET', 'PUT', 'DELETE'])
+def get_or_create_or_delete_todo(todoid=None):
+    from models import Todo
+    if todoid == None and request.method =='GET':
+        return Todo.get_todos()
+    elif todoid == None:
+        body = request.json['body'] 
+        priority = request.json['priority']
+        completed = request.json['completed']
+        return Todo.create_todo(body, priority, completed)  #POST method or create todo happens here
+    elif todoid and request.method == 'DELETE':
+        return Todo.delete_todo(todoid)
+    else:
+        return Todo.get_todo(todoid)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=DEBUG, port=PORT)
+
